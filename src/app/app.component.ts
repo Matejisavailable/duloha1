e27f9a3644048a79c225e1a8604a9ea552caec7f
Jakub Gonda <jakubgonda158@gmail.com>
2022-02-21 17:08:04 +0100

    rozdelie stringu do pola a nastavenie checkboxov

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,29 @@ export class AppComponent {
   title = 'duloha1';
   desiatkove = 0;
   dvojkove = "";
-  public prevodDo2(){
-    this.dvojkove = ((this.desiatkove >>> 0).toString(2));
+  i = 0;
+  pom = 1;
+
+public prevodDo2() {
+  this.dvojkove = ((this.desiatkove >>> 0).toString(2));//1010101
+  let index = this.dvojkove.length;
+  var pole:number[] = new Array(index);
+  var pole_pom:number[] = new Array(index);
+  let pom = 1;
+  for (let i = 0; i < this.dvojkove.length; i++){
+    var str = this.dvojkove;
+    var sliced = str.slice(index-1,index);
+    pole[i] = Number(sliced);
+    pole_pom[index-1] = pole[i];
+    let select = document.getElementById(String(pom));
+    if( pole_pom[index]==1){
+      // select.ariaChecked = "true";
+    }
+    pom++;
+    index--;
   }
+  alert(pole_pom);
+}
+
+
 }
